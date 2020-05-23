ed704cb37984b9e8e272a79f3060639662f13d5a
tcim <tcim4896@gmailcom>
2020-05-23 16:59:24 +0100

    Save, and some resource files..

---
+++ res/textspan/res/src.js
@@ -0,0 +1,26 @@
+function replace(str,term,replacement){
+	let r="",len=string=>string.length;
+	let m={
+		indexes:[],
+		match:[]
+	};
+    for(let i=0;i<len(str);i++){
+        for(let x=0;x<len(term);x++){
+         	if(str[i+x]==term[x]){
+				m.indexes.push(i+x)
+				if(len(m.indexes)==len(term)){
+					m.match.push(i)
+					r+=replacement;
+					x=0;
+					i+=len(term);
+				}
+         	}else{
+         		m.indexes=[];
+         	}
+        }
+       	if(typeof str[i] !== "undefined"){
+       		r+=str[i];
+       	}
+    }
+    return r;
+}
