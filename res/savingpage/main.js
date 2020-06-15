9a080af8e5899f99f17a5e03a26c9e6ec4841239
tcim <tcim4896@gmailcom>
2020-06-15 14:23:44 +0100

    Saving html implementation..

---
+++ res/savingpage/main.js
@@ -0,0 +1,156 @@
+cl=console.log;
+root=document.querySelector("[root]");
+mState = {
+	services:{
+		order:[],
+		cursor:{
+			dragging: false,
+			pos:0,
+		},
+		x:0,
+		y:0,
+	}
+};
+_=mState.services;
+function stateChange(){}
+
+function o(args) {
+	let r = document.createElement("div");
+	for(let prop in args){
+		switch(prop){
+			case "text":
+				r.textContent = args[prop];
+			break;
+			case "editable":
+				r.setAttribute("contentEditable", args[prop])
+			break;
+			case "class":
+				r.setAttribute("class", args[prop]);
+			break;
+			case "siblings":
+				for (let sibling of args[prop]) {
+    			r.appendChild(sibling);
+  			}
+  			break;
+  			case "id":
+  				mState[args[prop]] = r;
+  			break;
+  			case "placeholder":
+  				r.setAttribute("data-placeholder",args[prop]);
+  			break;
+  			default:
+  				r.setAttribute("class", "input")
+  			break;
+		}
+	}
+  return r;
+}
+
+function e(elm,type,fn) {
+	function ffkeys(event){ // key validator
+		let r;
+		switch(event.key){
+			case "Shift":
+				r="";
+			break;
+			case "Tab":
+				r="";
+			break;
+			case "Backspace":
+				r="";
+			break;
+			// Ctrl, Alt -> dumpfile[valid]
+			default:
+				r=event.key;
+			break;
+		}
+		return r;
+	}
+
+	if(true){ // if elm.ffkeys
+		elm.addEventListener(type,function(e){
+			e.value=ffkeys(e);
+			fn.call(e)
+		});
+	}else{
+		elm.addEventListener(type,fn)
+	}
+  return elm;
+}
+
+function s(id) {
+  return mState[id];
+}
+
+function b(elm, sibling) {
+  elm.appendChild(sibling);
+}
+
+e(document.body,"mousedown",function(e){
+	mState['mousedown']=true
+})
+
+e(document.body,"mouseup",function(e){
+	mState['mousedown']=false
+})
+
+function input(placeholder){
+	let div=document.createElement("div");
+	div.input=true;
+	div.setAttribute("contentEditable",true)
+	div.setAttribute("class","input")
+	// div.setAttribute("data-text",placeholder+"...")
+	// clear input..
+	return div;
+}
+
+function btn(text){
+	let div=document.createElement("div");
+	div.innerText=text
+	div.setAttribute("class","btn")
+	return div;
+}
+
+function text(text){
+	let div=document.createElement("div");
+	div.innerText=text
+	div.setAttribute("class","btn")
+	return div;
+}
+
+function dropdown(props){
+	let dropdown=document.createElement("div");
+	dropdown.open=false;
+  	dropdown.textContent=props.text;
+	dropdown.setAttribute("class","dropdown");
+  	dropdown.addEventListener("click",function(e){
+	    s(props.id).open=!s(props.id).open;
+	    if(!s(props.id).open){
+	    	s(props.id).style.height="20px";
+	    }else{
+	    	s(props.id).style.height=(props.options.length+1)*20+"px";
+	    }
+ 	})
+ 	let icon=document.createElement("div");
+	icon.setAttribute("class","icon")
+	icon.innerHTML="&#x25BC";
+ 	dropdown.appendChild(icon);
+
+	for(let option of props.options){
+		let opt=document.createElement("div");
+		opt.setAttribute("class","option")
+		opt.textContent=option.text;
+		opt.addEventListener("click",function(e){
+			s(props.id).value=option.value;
+		})
+    	dropdown.appendChild(opt);
+	}
+  	mState[props.id]=dropdown;
+	return dropdown;
+}
+
+b(root,input())
+b(root,btn("add image"))
+b(root,o({id:"wrapper",class:"wrapper",editable:true}))
+
+stateChange() //init
