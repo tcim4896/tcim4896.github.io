b0b433aa7c9d0a427fba49cb0a6b8b040a62f56b
tcim <tcim4896@gmailcom>
2020-06-24 20:32:18 +0100

    Another implementation method..

--- res/dragtoorder/impl3/main.js
+++ res/dragtoorder/impl3/main.js
@@ -2,8 +2,15 @@ cl=console.log;
 root=document.querySelector("[root]");
 mState = {
 	services:{
+		items:{
+			list:["item1","item2","item3","item4","item5"],
+		},
 		cursor:{
+			target:undefined,
 			dragging: false,
+			pos:0,
+			layerX:0,
+			layerY:0,
 		},
 		x:0,
 		y:0,
@@ -11,7 +18,17 @@ mState = {
 };
 _=mState.services;
 
-function stateChange(){}
+// Propchaining
+d={
+  value:{},
+  fn(prop){
+    this.value[prop]=prop;
+    return this;
+  }
+};
+
+d.fn(0).fn(1)
+
 
 function o(args) {
 	let r = document.createElement("div");
@@ -147,66 +164,143 @@ function dropdown(props){
 	return dropdown;
 }
 
-items=["item1","item2","item3","item4","item5"];
-
 console.log("Welcome!")
+function stateChange(obj){
+	function init(){
+		for(let services in _){
+			for(let service in _[services]){
+				if(typeof _[services][service]!=="undefined"&&
+					typeof _[services][service].handler=="function"){
+					let vars=_[services][service].variables;
+					// service({prop:1,prop:2})
+					// cl(vars);
+					// cl(_[services][vars])
+					_[services][service].handler(_[services][vars])
+				}
+			}
+		}
+	}
+	function update(obj){
+		// cl("update",obj)
+		// cl(_[obj.service])
+		// cl(_[obj.service][obj.fnCall])
+		//cl(_[obj.service][obj.fnCall].handler(_[obj.service][obj.var]))
+	}
+
+	if(obj=="init"){
+		init();
+	}else{
+		update(obj)
+	}
+
+}
+
+b(root,o({id:"wrapper",class:"wrapper"}))
+
+function p(elm,props){
+	for(let prop in props){
+		elm.setAttribute(prop,props[prop])
+	}
+	return elm;
+}
+
 e(document.documentElement,"mousemove",function(){
 	_.x=this.clientX;
 	_.y=this.clientY;
-	//cl(_.x,_.y)
+	//cl("x",_.x,"y",_.y)
+	if(_.cursor.dragging==true){
+		_.cursor.target.style.left=_.x-_.cursor.layerX+"px";
+		_.cursor.target.style.top=_.y-_.cursor.layerY+"px";
+	}
 
-	if(_.cursor.dragging==true&&
-		typeof _.cursor.target == "object"){
-		_.cursor.target.style.left=_.x-_.cursor.x+"px";
-		_.cursor.target.style.top=_.y-_.cursor.y+"px";
+	for(let child of s("wrapper").children){
+		// store future target position
+		let item=_.cursor.target;
+		if(typeof item!=="undefined"){
+			if(item.offsetTop>child.offsetTop&&
+				item.offsetTop<child.offsetTop+
+						child.offsetHeight){
+				cl(_.cursor.target.textContent,child.textContent)
+				st(child,{
+					backgroundColor:"#efefef",
+				})
+				child.innerHTML="Place here.."
+			}
+		}
+
+		/*
+			leaving child
+		*/
 	}
 })
 
 e(document.documentElement,"mouseup",function(){
 	cl("mouseup")
-	_.cursor.dragging==false;
+	_.cursor.dragging=false;
+
 	if(typeof _.cursor.target=="object"){
-		// replace dummy with cursor target
-		
 		_.cursor.target.style.position="relative";
 		_.cursor.target.style.left="auto";
 		_.cursor.target.style.top="auto";
-		_.cursor.target=undefined;
 	}
+
+	stateChange({
+		service:"items",
+		var:["list"],
+		fnCall:"listItems",
+	})
+
+	s("dummy").remove()
 })
 
-b(root,o({id:"wrapper",class:"wrapper"}))
-let i=0;
-for(let item of items){
-	b(s("wrapper"),
-		e(o({id:i,text:item,class:"item"}),"mousedown",function(){
-			cl("mousedown")
-			 this.target.style.position="fixed";
-			 this.target.style.width="200px";
-			_.cursor={
-				target:this.target,
-				dragging:true,
-				y:this.layerY,
-				x:this.layerX,
-			};
-			_.cursor.target.style.left=_.x-_.cursor.x+"px";
-			_.cursor.target.style.top=_.y-_.cursor.y+"px";
-			_.cursor.target
-				.insertAdjacentElement('beforeBegin',o({
-					id:"dummy",
-					class:"dummy"
-				}));
-		})
-	)
-	i++;
+function c(node,id){
+	let elm=node.cloneNode(true);
+	mState[id]=elm;
+	return elm;
 }
-/*
-	drag item
-	item xy meets another item
-	change array according
-	update view(still dragging)
-	mouse up xy
-	update array according
-	update view
-*/
-stateChange() //init
+
+function st(elm,styles){
+	for(let prop in styles){
+		elm.style[prop]=styles[prop];
+	}
+	return elm;
+}
+
+function registerService(serviceId,variables,handler){//name, handler.parameters handler.fn
+	_[serviceId][handler.name]={
+		variables,
+		handler,
+	};
+}
+
+registerService("items","list",listItems)
+
+function listItems(items){// service({prop:1,prop:2})
+	s("wrapper").innerHTML="";
+	for(let i=0; i<items.length;i++){
+		b(s("wrapper"),
+			e(o({text:items[i],class:"item"}),"mousedown",function(){
+				this.target
+					.insertAdjacentElement('beforeBegin',c(this.target,"dummy"));
+
+				cl("mousedown")
+				this.target.style.position="fixed";
+			 	//this.target.style.width="200px";
+
+				_.cursor={
+					target:this.target,
+					dragging:true,
+					layerY:this.layerY,
+					layerX:this.layerX,
+				};
+
+				this.target.style.left=_.x-this.layerX+"px";
+				this.target.style.top=_.y-this.layerY+"px";
+			})
+
+		)
+	}
+}
+
+
+stateChange("init") //init
