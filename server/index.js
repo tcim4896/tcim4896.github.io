df95830061e201fb5750cc8142edaf37e8b1eef3
tcim <tcim4896@gmail.com>
2020-08-31 19:52:54 +0200

    Basic account creation,login,listing..

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 8080;
+const http = require('http');
+const bodyParser = require('body-parser')
+const app = express()
+const fs = require("fs")
+
+app.use(bodyParser.json());
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.header('Access-Control-Allow-Credentials', true);
+    next();
+});
+
+const { spawn } = require('child_process');
+
+app.use('/', express.static(path.join(__dirname, 'dist')))
+ 
+app.all('*', (req, res) => { 
+    res.status(200).sendFile(path.join(__dirname, 'dist')+ "/index.html");
+});
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
